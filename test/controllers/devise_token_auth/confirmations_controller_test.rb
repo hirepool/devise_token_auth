c34e1f6750e270f2dc0755c689b77fa3db18aa99
Evan Marell <evan.marell@gmail.com>
2018-04-19 17:32:16 -0700

    Test confirmations with a confirm_within expiration

--- test/controllers/devise_token_auth/confirmations_controller_test.rb
+++ test/controllers/devise_token_auth/confirmations_controller_test.rb
@@ -48,6 +48,37 @@ class DeviseTokenAuth::ConfirmationsControllerTest < ActionController::TestCase
           subject
           assert_redirected_to(/^#{success_url}/)
         end
+
+        describe 'within an explicit confirmation period' do
+          let(:user) { users(:recent_unconfirmed_email_user) } # confirmation_sent_at => 1.day.ago
+
+          before { Devise.confirm_within = 1.week } # 1.week.from(1.day.ago) > now
+          after { Devise.confirm_within = nil } # nil = unlimited
+
+          it 'should confirm the user with the given token' do
+            assert_changes -> { user.confirmed_at }, from: nil do
+              subject
+              user.reload
+            end
+          end
+
+          it 'should create a new auth_token for the user with the given token' do
+            assert_changes -> { user.tokens } do
+              subject
+              user.reload
+            end
+          end
+
+          it 'should sign the user in' do
+            subject
+            assert warden.authenticated?(:user)
+          end
+
+          it 'should redirect to success url' do
+            subject
+            assert_redirected_to(/^#{success_url}/)
+          end
+        end
       end
 
       describe 'failure' do
@@ -99,6 +130,34 @@ class DeviseTokenAuth::ConfirmationsControllerTest < ActionController::TestCase
           it 'should include error "confirmation token is invalid" in the json response' do
             subject
             assert_includes json_response['errors']['confirmation_token'], 'is invalid'
+          end
+        end
+
+        describe 'for an expired confirmation period' do
+          let(:confirmation_period_expired_message) do
+            "needs to be confirmed within %{period}, please request a new one" \
+              % {period: Devise::TimeInflector.time_ago_in_words(1.week.ago)}
+          end
+
+          # user.confirmation_sent_at = 2.weeks.ago (see :unconfirmed_email_user fixture).
+          before { Devise.confirm_within = 1.week } # 1.week.from(2.weeks.ago) < now
+          after { Devise.confirm_within = nil } # nil = unlimited
+
+          subject { get :show, params: valid_params, xhr: true }
+
+          it 'should respond with 422 "Unprocessable Entity"' do
+            subject
+            assert_response :unprocessable_entity
+          end
+
+          it 'should have "success: false" in the json response' do
+            subject
+            assert_equal false, json_response['success']
+          end
+
+          it 'should include error "needs to be confirmed within..." in the json response' do
+            subject
+            assert_includes json_response['errors']['email'], confirmation_period_expired_message
           end
         end
       end
